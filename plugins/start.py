0dbc6f2ef0a329da12608b2b714663a86a5b4cca
kmacprt <72274248+kmacprt@users.noreply.github.com>
2021-01-01 01:33:48 -0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,7 +7,7 @@ async def start(client, message):
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("🔔 නාළිකාව", url="https://t.me/lkhitech")],
         [InlineKeyboardButton(
-            "😊සම්බන්ධ කරගන්න ", url="https://t.me/kavinduaj")]
+            "😊සම්බන්ධ කරගන්න ", url="https://t.me/kavinduaj")],
         [InlineKeyboardButton("🖥 ප්‍රවේශ වන්න 😊", url="https://visi.tk/kavinduaj")]
     ])
     welcomed = f"ආයුබෝවාන්🙏<b>{message.from_user.first_name}</b>\n/help වැඩි දුර තොරතුරැ"
